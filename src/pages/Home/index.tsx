72d62abaf9123ef871e9389e31e64a5eb707cde2
Arunram07 <arunram@zevocorp.in>
2022-03-21 18:21:32 +0530

    choke :ui

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
 
 import "./Home.scss";
-import Stats from "./Stats";
 import Transfer from "./Transfer";
 import { ReactComponent as Close } from "../../assets/icons/close.svg";
 
 const Home: React.FC = () => {
   const [alertInfo, setAlertInfo] = useState(true);
-  const [state, setState] = useState({ amount: "0.1", currency: "eth" });
   return (
     <>
       {alertInfo && (
@@ -21,11 +19,8 @@ const Home: React.FC = () => {
           </div>
         </div>
       )}
-      <button onClick={() => setState({ ...state, amount: "1" })}>1 eth</button>
-      <button onClick={() => setState({ ...state, amount: "0.1" })}>0.1 eth</button>
       <div className="home">
         <Transfer />
-        <Stats amount={state.amount} currency={state.currency} />
       </div>
     </>
   );
